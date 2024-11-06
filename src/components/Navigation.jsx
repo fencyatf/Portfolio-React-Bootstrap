bf6f6491df646ad37c8a5f8c2bcc6e099a1e30b1
fency <fencyatf@gmail.com>
2024-11-06 23:16:36 +0530

    slight change in navbar section

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ import './Navigation.css';
 const Navigation = () => {
   return (
     <div className="main">
-      <Navbar expand="md" className='p-4' style={{ backgroundColor: 'black' }}>
+      <Navbar expand="md" className='p-4' fixed="top" style={{ backgroundColor: ' black' }}>
         <Navbar.Brand href="#home">
           <Image className='img-logo' src="https://i.pinimg.com/236x/de/8b/7f/de8b7fbfb2aa13430071e5f605eb4f63.jpg" rounded/>
         </Navbar.Brand>
